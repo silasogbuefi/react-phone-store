007444c63e63c1c9210fbd2e261507e5dda4edb3
silas ogbuefi <eniolaadebayo24@gmail.com>
2020-08-13 11:56:34 +0100

    adding a detail component

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {storeProducts, detailProduct} from './data';
+import { ThemeProvider } from 'styled-components';
 
 //contextAPI
 const ProductContext = React.createContext();
@@ -10,16 +11,53 @@ const ProductContext = React.createContext();
 
  class ProductProvider extends Component {
      state = {
-         products: storeProducts,
-         detailProduct: detailProduct
+         products: [],
+         detailProduct: detailProduct,
+         cart: []
      }
-     handleDetail = ()=> {
-         console.log('hello from detail');
+     componentDidMount() {
+         this.setProducts();
+     }
+     setProducts = () =>{
+         let tempProducts = [];
+         storeProducts.forEach(item => {
+             const singleItem = {...item};
+             tempProducts = [...tempProducts,singleItem];
+         })
+         this.setState(() => {
+             return {products: tempProducts};
+         });
+     };
+
+     getItem = (id) => {
+         const product = this.state.products.find(item => item.id ===id);
+         return product;
      }
 
-     addToCart = ()=> {
-         console.log('hello fromadd to cart');
-     }
+     handleDetail = (id)=> {
+         const product = this.getItem(id);
+         this.setState(() => {
+             return {detailProduct:product}
+         })
+     };
+
+     addToCart = id=> {
+         let tempProducts =[...this.state.products];
+         const index = tempProducts.indexOf(this.getItem(id));
+         const product = tempProducts[index];
+         product.inCart = true;
+         product.count =1;
+         const price = product.price;
+         product.total = price;
+
+         this.setState(()=> {
+             return {products: tempProducts, cart:[...this.state.cart,
+            product]}
+         },() => {console.log(this.state);
+        })
+
+     };
+
     render() {
         return (
            <ProductContext.Provider value={{
